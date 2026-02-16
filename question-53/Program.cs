9815d5acd9cbc7c0a6b9d6ccf9cf5f5830bdf3b6
Darius Pop <dariuscpop@gmail.com>
2026-02-16 09:59:27 +0000

    Completed question 53

---
+++ question-53/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    public static int MaxSubArray(int[] nums) {
+        int maxSum = nums[0], currentSum = nums[0];
+        for (int index = 1; index < nums.Length; index++)
+        {
+            if (currentSum + nums[index] > nums[index])
+                currentSum += nums[index];
+            else
+                currentSum = nums[index];
+            if (currentSum > maxSum)
+                maxSum = currentSum;
+        }
+        return maxSum;
+    }
+    static void Main(string[] args)
+    {
+        int[] nums = new int[] { 5, 4, -1, 7, 8 };
+        Console.WriteLine(MaxSubArray(nums));
+    }
+}
