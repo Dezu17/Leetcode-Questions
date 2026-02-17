afd893f18a54c833e0bbacfe77c1d12c6f7734ac
Darius Pop <dariuscpop@gmail.com>
2026-02-17 21:38:22 +0000

    Completed question 347

---
+++ question-347/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+public class Program
+{
+    public static int[] TopKFrequent(int[] nums, int k) {
+        Hashtable frequency = new Hashtable();
+        for (int index = 0; index < nums.Length; index++)
+            if (!frequency.Contains(nums[index]))
+                frequency.Add(nums[index], 1);
+            else {
+                int count = (int)frequency[nums[index]];
+                frequency[nums[index]] = ++count;
+            }
+        PriorityQueue<int, int> orderedElements = new PriorityQueue<int, int>();
+        foreach (DictionaryEntry current in frequency)
+            orderedElements.Enqueue((int)current.Key, (int)current.Value * -1);
+        int[] result = new int[k];
+        for (int count = 0; count < k; count++)
+            result[count] = orderedElements.Dequeue();
+        return result;
+    }
+    public static void Main(String[] args)
+    {
+        int[] nums = {1,2,1,2,1,2,3,1,3,2};
+        int k = 2;
+        int[] result = TopKFrequent(nums, k);
+        for (int index = 0; index < result.Length; index++)
+            Console.Write(result[index] + " ");
+    }
+}
