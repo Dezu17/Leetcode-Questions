5ab5dd4e25df670f8ae32b57d8cd32f717500224
Dezu17 <dariuscpop@gmail.com>
2026-02-18 20:38:11 +0000

    Completed question 560

---
+++ question-560/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static int SubarraySum(int[] nums, int k)
+    {
+        Dictionary<int, int> frequencies = new Dictionary<int, int>();
+        int prefixSum = 0, result = 0;
+        for (int index = 0; index < nums.Length; index++)
+        {
+            prefixSum += nums[index];
+            if (prefixSum == k)
+                result++;
+            if (frequencies.ContainsKey(prefixSum - k))
+                result += frequencies[prefixSum - k];
+            if (!frequencies.ContainsKey(prefixSum))
+                frequencies.Add(prefixSum, 0);
+            frequencies[prefixSum]++;
+        }
+        return result;
+    }
+    public static void Main(string[] args)
+    {
+        int[] nums = { 1, 1, 1 };
+        int k = 2;
+        Console.WriteLine(SubarraySum(nums, k));
+    }
+}
