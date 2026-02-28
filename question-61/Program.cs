b799a85671fb6332746bf27bc22217073bff9b89
Dezu17 <dariuscpop@gmail.com>
2026-02-28 22:18:32 +0000

    Completed question 61

---
+++ question-61/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    public static ListNode RotateRight(ListNode head, int k)
+    {
+        if (k == 0)
+            return head;
+        ListNode copy = head;
+        int length = 0;
+        while (copy != null)
+        {
+            copy = copy.next;
+            length++;
+        }
+        if (k == 0)
+            return null;
+        int elementsToSkip = length - k % length - 1;
+        copy = head;
+        while (elementsToSkip-- > 0)
+            copy = copy.next;
+        ListNode end = copy;
+        while (end.next != null)
+            end = end.next;
+        end.next = head;
+        head = copy.next;
+        copy.next = null;
+        return head;
+    }
+    public static void Main(string[] args)
+    {
+        ListNode head = new ListNode();
+        head = RotateRight(null, 1);
+        while (head != null)
+        {
+            Console.WriteLine(head.val);
+            head = head.next;
+        }
+    }
+}
