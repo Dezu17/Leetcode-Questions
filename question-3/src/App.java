2ccfef115beb196062cc69eaaedb35dfdc00823a
Dezu17 <dariuscpop@gmail.com>
2026-08-23 11:37:15 +0100

    Completed question 3

--- question-3/src/App.java
+++ question-3/src/App.java
@@ -1,38 +1,35 @@
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class App {
     public static int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Boolean> characterVisited = new HashMap<>();
+        if (s.length() == 0)
+            return 0;
+        HashSet<Character> visited = new HashSet<>();
+        visited.add(s.charAt(0));
         int maxLength = 0;
-        int left = 0, right = 0, currentLength = 0;
-        while (right < s.length()) {
-            char currentCharacter = s.charAt(right);
-            if (!characterVisited.containsKey(currentCharacter) || !characterVisited.get(currentCharacter)) {
-                characterVisited.put(currentCharacter, true);
-                currentLength++;
-            } else {
-                if (currentLength > maxLength)
-                    maxLength = currentLength;
-                boolean passedRepeatingCharacter = false;
-                while (left < right && !passedRepeatingCharacter) {
-                    char leftCharacter = s.charAt(left);
-                    if (leftCharacter == currentCharacter)
-                        passedRepeatingCharacter = true;
-                    characterVisited.put(leftCharacter, false);
-                    left++;
+        int startIndex = 0, endIndex = 1;
+        while (endIndex < s.length()) {
+            char currentCharacter = s.charAt(endIndex);
+            if (visited.contains(currentCharacter)) {
+                if (maxLength < endIndex - startIndex)
+                    maxLength = endIndex - startIndex;
+                while (s.charAt(startIndex) != currentCharacter) {
+                    visited.remove(s.charAt(startIndex));
+                    startIndex++;
                 }
-                currentLength = right - left + 1;
-                characterVisited.put(currentCharacter, true);
+                startIndex++;
             }
-            right++;
+            else
+                visited.add(currentCharacter);
+            endIndex++;
         }
-        if (currentLength > maxLength)
-            return currentLength;
+        if (maxLength < endIndex - startIndex)
+                    maxLength = endIndex - startIndex;
         return maxLength;
     }
 
     public static void main(String[] args) {
-        String s = "abcabdbb";
+        String s = "";
         System.out.print("Result: " + lengthOfLongestSubstring(s));
     }
 }
