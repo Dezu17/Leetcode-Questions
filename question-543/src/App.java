9adad51164b9c6dfdcf02227512117bf73f4188a
Dezu17 <dariuscpop@gmail.com>
2026-09-20 20:00:23 +0100

    Completed question 543

--- question-543/src/App.java
+++ question-543/src/App.java
@@ -1,51 +1,41 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class App {
-
-    public static int maxDepth(TreeNode root) {
-        if (root != null) {
-            int left = 1 + maxDepth(root.left);
-            int right = 1 + maxDepth(root.right);
-            return Math.max(left, right);
-        }
-        return 0;
+    public static int maxDiameter;
+    
+    public static int diameterOfBinaryTree(TreeNode root) {
+        if (root == null)
+            return -1;
+        int leftDiameter = 1 + diameterOfBinaryTree(root.left);
+        int rightDiameter = 1 + diameterOfBinaryTree(root.right);
+        if (maxDiameter < leftDiameter + rightDiameter)
+            maxDiameter = leftDiameter + rightDiameter;
+        return Math.max(leftDiameter, rightDiameter);
     }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1,
+            new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+            new TreeNode(3));
 
-    public static void printTree(TreeNode root) {
-        if (root == null) {
-            System.out.println("[]");
-            return;
-        }
+        maxDiameter = 0;
+        System.out.println("root = [1,2,3,4,5]");
+        diameterOfBinaryTree(root);
+        System.out.println("diameter = " + maxDiameter);
 
-        Queue<TreeNode> nodes = new LinkedList<>();
-        nodes.add(root);
-        System.out.print("[");
-        boolean first = true;
-        while (!nodes.isEmpty()) {
-            TreeNode node = nodes.remove();
-            if (!first) {
-                System.out.print(",");
-            }
-            System.out.print(node.val);
-            first = false;
-            if (node.left != null) {
-                nodes.add(node.left);
-            }
-            if (node.right != null) {
-                nodes.add(node.right);
-            }
-        }
-        System.out.println("]");
-    }
+        root = new TreeNode(1,
+            null,
+            new TreeNode(2,
+                new TreeNode(3, new TreeNode(5), null),
+                new TreeNode(4)));
 
-    public static void main(String[] args) throws Exception {
-        TreeNode root = new TreeNode(3,
-            new TreeNode(9),
-            new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        maxDiameter = 0;
+        System.out.println("root = [1,null,2,3,4,5]");
+        diameterOfBinaryTree(root);
+        System.out.println("diameter = " + maxDiameter);
 
-        System.out.print("root = ");
-        printTree(root);
-        System.out.println("max depth = " + maxDepth(root));
+        root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+
+        maxDiameter = 0;
+        System.out.println("root = [1,2,3]");
+        diameterOfBinaryTree(root);
+        System.out.println("diameter = " + maxDiameter);
     }
 }
