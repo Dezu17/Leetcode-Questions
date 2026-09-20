51d477e8c735a05fd776469fbc93b2f352a80695
Dezu17 <dariuscpop@gmail.com>
2026-09-20 20:00:16 +0100

    Completed question 62

--- question-62/src/App.java
+++ question-62/src/App.java
@@ -1,16 +1,27 @@
 public class App {
-    private static int factorial(int n) {
-        int result = 1;
-        for (int count = 2; count <= n; count++)
-            result *= count;
-        return result;
-    }
-
-    public static int uniquePaths(int m, int n) {
-        return factorial(m + n - 2) / (factorial(m - 1) * factorial(n - 1));
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int index = 0; index < n; index++)
+            dp[0][index] = 1;
+        for (int index = 0; index < m; index++)
+            dp[index][0] = 1;
+        for (int row = 1; row < m; row++)
+            for (int column = 1; column < n; column++)
+                dp[row][column] = dp[row - 1][column] + dp[row][column - 1];
+        return dp[m - 1][n - 1];
     }
 
     public static void main(String[] args) {
-        System.out.println(uniquePaths(80, 2));
+        int[][] tests = {
+            {3, 7},
+            {3, 2},
+            {3, 6},
+            {3, 3}
+        };
+
+        App app = new App();
+        for (int[] test : tests) {
+            System.out.println(app.uniquePaths(test[0], test[1]));
+        }
     }
 }
