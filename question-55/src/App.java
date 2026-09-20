c827b302a4583c2d4129f54888584a27947a01b0
Dezu17 <dariuscpop@gmail.com>
2026-09-20 20:00:16 +0100

    Completed question 55

--- question-55/src/App.java
+++ question-55/src/App.java
@@ -1,23 +1,24 @@
 public class App {
-    public static boolean canJump(int[] nums) {
-        int max = 0;
-        for (int index = 0; index < nums.length; index++) {
-            if (max < index)
-                return false;
-            else {
-                if (max < index + nums[index])
-                    max = nums[index] + index;
-            }
-            if (max > nums.length - 1)
-                return true;
-        }
-        if (max > nums.length - 2)
-            return true;
-        return false;
+    public boolean canJump(int[] nums) {
+        int lastPosition = nums.length - 1;
+        for (int index = nums.length - 2; index >= 0; index--)
+            if (lastPosition <= index + nums[index])
+                lastPosition = index;
+        return lastPosition == 0 ? true : false;
     }
 
     public static void main(String[] args) {
-        int[] nums = { 0 };
-        System.out.println(canJump(nums));
+        App app = new App();
+
+        int[][] tests = {
+            {2, 3, 1, 1, 4},
+            {3, 2, 1, 0, 4},
+            {1, 2, 0, 1, 0},
+            {1, 2, 1, 0, 1}
+        };
+
+        for (int[] nums : tests) {
+            System.out.println(app.canJump(nums));
+        }
     }
 }
