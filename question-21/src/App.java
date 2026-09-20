7a37c5973edae4fbd06fc9fa40ed03540ed2b736
Dezu17 <dariuscpop@gmail.com>
2026-09-20 20:00:15 +0100

    Completed question 21

--- question-21/src/App.java
+++ question-21/src/App.java
@@ -1,14 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 != null && list2 != null) {
-            if (list1.val < list2.val)
-                return new ListNode(list1.val, mergeTwoLists(list1.next, list2));
-            else
-                return new ListNode(list2.val, mergeTwoLists(list1, list2.next));
-        } else if (list1 != null)
-            return list1;
-        else
-            return list2;
+        ListNode head = null, last = null;
+        while (list1 != null && list2 != null) {
+        ListNode newNode = new ListNode();
+        if (list1.val > list2.val) {
+            newNode.val = list2.val;
+            list2 = list2.next;
+        }
+        else {
+            newNode.val = list1.val;
+            list1 = list1.next;
+        }
+        if (head == null) {
+            head = newNode;
+            last = newNode;
+        }
+        else {
+            last.next = newNode;
+            last = last.next;
+        }
+        }
+        while (list1 != null) {
+        ListNode newNode = new ListNode(list1.val);
+        list1 = list1.next;
+        if (head == null) {
+            head = newNode;
+            last = newNode;
+        }
+        else {
+            last.next = newNode;
+            last = last.next;
+        }
+        }
+        while (list2 != null) {
+        ListNode newNode = new ListNode(list2.val);
+        list2 = list2.next;
+        if (head == null) {
+            head = newNode;
+            last = newNode;
+        }
+        else {
+            last.next = newNode;
+            last = last.next;
+        }
+        }
+        return head;
     }
 
     public static void main(String[] args) {
