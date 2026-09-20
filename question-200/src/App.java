befc2cdd3a1aaf221719b9de222023e79a794bb4
Dezu17 <dariuscpop@gmail.com>
2026-09-20 20:18:18 +0100

    Updated question 200

--- question-200/src/App.java
+++ question-200/src/App.java
@@ -1,33 +1,50 @@
 public class App {
-    private static void dfs(char[][] grid, boolean[][] visited, int row, int column) {
-        if (row >= 0 && row < grid.length && column >= 0 && column < grid[0].length &&
-            grid[row][column] == '1') {
-            visited[row][column] = true;
+    private void handleIsland(char[][] grid, int row, int column) {
+        if (row >= 0 && row < grid.length && column >= 0 && column < grid[0].length && grid[row][column] == '1') {
             grid[row][column] = '0';
-            dfs(grid, visited, row - 1, column);
-            dfs(grid, visited, row, column - 1);
-            dfs(grid, visited, row, column + 1);
-            dfs(grid, visited, row + 1, column);
+            handleIsland(grid, row + 1, column);
+            handleIsland(grid, row - 1, column);
+            handleIsland(grid, row, column + 1);
+            handleIsland(grid, row, column - 1);
         }
     }
-    public static int numIslands(char[][] grid) {
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
-        int count  = 0;
+
+    public int numIslands(char[][] grid) {
+        int islandCount = 0;
         for (int row = 0; row < grid.length; row++)
             for (int column = 0; column < grid[0].length; column++)
                 if (grid[row][column] == '1') {
-                    count++;
-                    dfs(grid, visited, row, column);
+                    handleIsland(grid, row, column);
+                    islandCount++;
                 }
-        return count;
+        return islandCount;
     }
+    
     public static void main(String[] args) {
-        char[][] grid = {
-            {'1','1','0','0','0'},
-            {'1','1','0','0','0'},
-            {'0','0','1','0','0'},
-            {'0','0','0','1','1'}
+        App app = new App();
+
+        char[][][] tests = {
+            {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+            },
+            {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+            },
+            {
+                {'1', '1', '0', '0', '1'},
+                {'1', '1', '0', '0', '1'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+            }
         };
-        System.out.println(numIslands(grid));
+
+        for (char[][] grid : tests)
+            System.out.println(app.numIslands(grid));
     }
 }
